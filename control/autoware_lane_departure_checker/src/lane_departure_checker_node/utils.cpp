c20fee401625055038625e872dc1bb0c62de28f5
Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2025-03-11 20:56:10 +0900

    remove nearest index search

--- control/autoware_lane_departure_checker/src/lane_departure_checker_node/utils.cpp
+++ control/autoware_lane_departure_checker/src/lane_departure_checker_node/utils.cpp
@@ -253,15 +253,7 @@ std::optional<std::vector<Segment2d>> convert_to_segments(
     return std::nullopt;
   }
 
-  // const auto nearest_index = motion_utils::findNearestIndex(boundary_points, target_point);
-  // if (nearest_index < 1) {
-  //   const auto & p1 = boundary_points.at(0);
-  //   const auto & p2 = boundary_points.at(1);
-  //   Segment2d segment(Point2d(p1.x, p1.y), Point2d(p2.x, p2.y));
-  //   return std::vector<Segment2d>{segment};
-  // }
   std::vector<Segment2d> segments;
-  // segments.reserve(nearest_index - 1);
 
   for (size_t i = 0; i < boundary_points.size() - 1; ++i) {
     const auto & curr = boundary_points[i];
