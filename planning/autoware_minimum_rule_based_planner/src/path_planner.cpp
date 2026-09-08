3cf574a2cd2bd0908fffea0e3eb0a4eccfebf011
Sugar-98 <154723489+Sugar-98@users.noreply.github.com>
2026-09-08 10:16:39 +0900

    fix(minimum_rule_based_planner): update route_deviation_stop to return nullopt (#3377)
    fix: update route_deviation_stop to retun nullopt
    
    Signed-off-by: Sugar-98 <154723489+Sugar-98@users.noreply.github.com>

--- planning/autoware_minimum_rule_based_planner/src/path_planner.cpp
+++ planning/autoware_minimum_rule_based_planner/src/path_planner.cpp
@@ -1439,16 +1439,9 @@ std::optional<PathWithLaneId> PathPlanner::plan_path(
 
   const lanelet::LaneletSequence lanelet_sequence(lanelets);
   const auto s = s_on_current_lanelet + lanelet::utils::getLaneletLength2d(backward_lanelets);
-  const auto [s_start, s_end] = [&]() {
-    Interval range = utils::refine_path_range(
-      {s - path_length_backward, s + path_length_forward}, lanelet_sequence, route_context_,
-      vehicle_info_, stop_margin);
-
-    if (route_deviation_stop(*current_lanelet_, current_pose, route_context_)) {
-      range.end = std::min(range.end, s);
-    }
-    return range;
-  }();
+  const auto [s_start, s_end] = utils::refine_path_range(
+    {s - path_length_backward, s + path_length_forward}, lanelet_sequence, route_context_,
+    vehicle_info_, stop_margin);
 
   if (s_end < s_start) {
     RCLCPP_WARN_THROTTLE(
@@ -1625,6 +1618,13 @@ std::optional<PathWithLaneId> PathPlanner::generate_path(
       RCLCPP_WARN(logger_, "Trajectory length too short after cropping: %f", trajectory->length());
       return std::nullopt;
     }
+  }
+
+  if (
+    !start_goal_planner_.start_planner_active() && !start_goal_planner_.goal_planner_active() &&
+    route_deviation_stop(*current_lanelet_, current_pose, route_context_)) {
+    RCLCPP_WARN(logger_, "Route deviation detected, cannot generate path");
+    return std::nullopt;
   }
 
   // Compose the polished path
--- planning/autoware_minimum_rule_based_planner/src/start_goal_planner/start_goal_planner.cpp
+++ planning/autoware_minimum_rule_based_planner/src/start_goal_planner/start_goal_planner.cpp
@@ -327,8 +327,7 @@ std::optional<PathPointTrajectory> StartGoalPlanner::plan(
 {
   const auto available_area = get_available_area(trajectory);
 
-  judge_goal_planner_act(trajectory, s_path_end, available_area, ego_pose);
-  judge_start_planner_act(current_lanelet, ego_pose);
+  judge_start_goal_planner_act(current_lanelet, trajectory, s_path_end, available_area, ego_pose);
 
   if (!start_planner_act_ && !goal_planner_act_) {
     return std::nullopt;  // StartGoalPlanner is not applied. normal termination
@@ -362,6 +361,25 @@ std::optional<PathPointTrajectory> StartGoalPlanner::plan(
   }
   generated_trajectory_ = refined_trajectory;
   return generated_trajectory_;
+}
+
+void StartGoalPlanner::judge_start_goal_planner_act(
+  const lanelet::ConstLanelet & current_lanelet, const PathPointTrajectory & trajectory,
+  const double & s_path_end, const std::vector<AvailableArea> & available_area,
+  const geometry_msgs::msg::Pose & ego_pose)
+{
+  judge_goal_planner_act(trajectory, s_path_end, available_area, ego_pose);
+  judge_start_planner_act(current_lanelet, ego_pose);
+
+  if (start_planner_act_ || goal_planner_act_) {
+    const bool ego_outside_available_area = !is_point_in_polygons(
+      autoware_utils_geometry::Point2d{ego_pose.position.x, ego_pose.position.y}, available_area);
+    if (ego_outside_available_area) {
+      start_planner_act_ = false;
+      goal_planner_act_ = false;
+      generated_trajectory_ = std::nullopt;
+    }
+  }
 }
 
 void StartGoalPlanner::judge_start_planner_act(
